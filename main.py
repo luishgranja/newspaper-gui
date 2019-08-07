55e28f03659410ddd5a48a52d933bc4db900a071
Luis Granja <luis.granja@correounivalle.edu.co>
2019-08-07 18:01:12 -0500

    fix: adding validations

--- main.py
+++ main.py
@@ -36,56 +36,96 @@ def crear_archivo(n, NOMBRES, MIN_PAG, MAX_PAG, LECTORES):
     elegidos = lineas[0][lineas[0].find('[')+1:lineas[0].find(']')].split(', ')
     num_paginas_elegidos = lineas[1][lineas[1].find('[')+1:lineas[1].find(']')].split(', ')
     num_lectores = lineas[2].split(' = ')[1].split(';')[0]
-    
-    resultado = 'Temas elegidos: \n\n'
-    nombres = ''
-    for i in range(len(elegidos)):
-        if(elegidos[i]=='true'):
-            nombres += str(NOMBRES[i].get()) + ' # Paginas: ' + num_paginas_elegidos[i] + '\n'
-    resultado += nombres + '\nPotencial lectores: ' + num_lectores
+
+    # Validacion para la respuesta del problema, si hay solucion la muestra, si no muestra un mensaje
+    if elegidos.count('false') == int(n):
+        resultado = 'No hay solución :('
+    else:
+        resultado = 'Temas elegidos: \n\n'
+        nombres = ''
+        for i in range(len(elegidos)):
+            if(elegidos[i]=='true'):
+                nombres += str(NOMBRES[i].get()) + ' # Paginas: ' + num_paginas_elegidos[i] + '\n'
+        resultado += nombres + '\nPotencial lectores: ' + num_lectores
     
     messagebox.showinfo(message=resultado, title="Resultado")
 
+    # Borrar los campos despues de haber mostrado la solucion
+    del NOMBRES[:]
+    del MIN_PAG[:]
+    del MAX_PAG[:]
+    del LECTORES[:]
+    # Limpia los inputs iniciales
+    limpiar_inputs()
+    # Destruye los elementos del frame de info del periodico
+    frame_inputs.destroy()
+
+def limpiar_inputs():
+    numero_temas.delete(0,END)
+    numero_temas.insert(0,0)
+
+    numero_paginas.delete(0,END)
+    numero_paginas.insert(0,0)
+
+'''
+n es el numero de temas que el usuario desea ingresar
+'''
 def crear_inputs(n):
-    tema_label = Label(frame_inputs, text="Tema")
-    tema_label.grid (row=1, column=1 , padx = 5 , pady = 5)
 
-    min_paginas_label = Label(frame_inputs, text="Número min paginas")
-    min_paginas_label.grid (row=1, column=2 , padx = 5 , pady = 5)
-
-    max_paginas_label = Label(frame_inputs, text="Número max paginas")
-    max_paginas_label.grid (row=1, column=3 , padx = 5 , pady = 5)
-
-    lectores_label = Label(frame_inputs, text="Lectores")
-    lectores_label.grid (row=1, column=4 , padx = 5 , pady = 5)
+    try:
+        numero = int(n)
+        int(numero_paginas.get())
+        if numero <= 0:
+            messagebox.showinfo(message='La cantidad de temas debe ser mayor a 0 (cero)', title="Error")
+            limpiar_inputs()
+        else:
+            
+            tema_label = Label(frame_inputs, text="Tema")
+            tema_label.grid (row=1, column=1 , padx = 5 , pady = 5)
     
-    NOMBRES = []
-    MIN_PAG = []
-    MAX_PAG = []
-    LECTORES = []
+            min_paginas_label = Label(frame_inputs, text="Número min paginas")
+            min_paginas_label.grid (row=1, column=2 , padx = 5 , pady = 5)
+    
+            max_paginas_label = Label(frame_inputs, text="Número max paginas")
+            max_paginas_label.grid (row=1, column=3 , padx = 5 , pady = 5)
+    
+            lectores_label = Label(frame_inputs, text="Lectores")
+            lectores_label.grid (row=1, column=4 , padx = 5 , pady = 5)
+            
+            NOMBRES = []
+            MIN_PAG = []
+            MAX_PAG = []
+            LECTORES = []
+    
+            for i in range(int(n)):
+                nombre_tema = Entry(frame_inputs, width="10")
+                nombre_tema.grid (row=2+i, column=1 , padx = 5 , pady = 5)
+                NOMBRES.append(nombre_tema)
+    
+                min_paginas = Entry(frame_inputs, width="5")
+                min_paginas.grid (row=2+i, column=2 , padx = 5 , pady = 5)
+                min_paginas.delete(0,END)
+                min_paginas.insert(0,0)
+                MIN_PAG.append(min_paginas)
+    
+                max_paginas = Entry(frame_inputs, width="5")
+                max_paginas.grid (row=2+i, column=3 , padx = 5 , pady = 5)
+                MAX_PAG.append(max_paginas)
+    
+                lectores = Entry(frame_inputs, width="5")
+                lectores.grid (row=2+i, column=4 , padx = 5 , pady = 5)
+                LECTORES.append(lectores)
+    
+    
+            boton_calcular = Button (frame_inputs , text="CALCULAR" ,command=lambda: crear_archivo(n, NOMBRES, MIN_PAG, MAX_PAG, LECTORES))
+            boton_calcular.grid (row=int(n)+2, column=1, padx = 5 , pady = 5)
+    
+            frame_inputs.pack()
 
-    for i in range(int(n)):
-        nombre_tema = Entry(frame_inputs, width="10")
-        nombre_tema.grid (row=2+i, column=1 , padx = 5 , pady = 5)
-        NOMBRES.append(nombre_tema)
-
-        min_paginas = Entry(frame_inputs, width="5")
-        min_paginas.grid (row=2+i, column=2 , padx = 5 , pady = 5)
-        MIN_PAG.append(min_paginas)
-
-        max_paginas = Entry(frame_inputs, width="5")
-        max_paginas.grid (row=2+i, column=3 , padx = 5 , pady = 5)
-        MAX_PAG.append(max_paginas)
-
-        lectores = Entry(frame_inputs, width="5")
-        lectores.grid (row=2+i, column=4 , padx = 5 , pady = 5)
-        LECTORES.append(lectores)
-
-
-    boton_calcular = Button (frame_inputs , text="CALCULAR" ,command=lambda: crear_archivo(n, NOMBRES, MIN_PAG, MAX_PAG, LECTORES))
-    boton_calcular.grid (row=int(n)+2, column=1, padx = 5 , pady = 5)
-
-    frame_inputs.pack()
+    except ValueError:
+        messagebox.showinfo(message='Debe ingresar un numero mayor a 0 (cero)', title="Error")
+        limpiar_inputs()
+    
 
 # Frame de entrada de informacion inicial del problema
 frameInfo = Frame(gui)
@@ -108,10 +148,8 @@ numero_paginas.grid (row=2, column=2 , padx = 5 , pady = 5)
 boton_crear = Button (frameInfo , text="CREAR" ,command=lambda: crear_inputs(numero_temas.get()))
 boton_crear.grid (row=3, column=1, padx = 5 , pady = 5)
 
-numero_temas.delete(0,END)
-numero_temas.insert(0,0)
+limpiar_inputs()
 
 frameInfo.pack()
 
 gui.mainloop()
-
